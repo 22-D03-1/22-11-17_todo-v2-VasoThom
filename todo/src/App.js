9add41703b8f15b6a9e5ce1d277cdb32abed293e
Vaso <vasothom@hotmail.com>
2022-11-17 20:37:09 +0100

    input and list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+
+      setTodo("");
+    }
+  };
+  const removeTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Todo APP</h1>
+        <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+        <TodoList list={todos} remove={removeTodo} />
       </header>
     </div>
   );
---
+++ todo/src/components/TodoInput.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function TodoInput({ todo, setTodo, addTodo }) {
+  return (
+    <div>
+      <input
+        type="text"
+        value={todo}
+        name="todo"
+        onChange={(e) => {
+          setTodo(e.target.value);
+          console.log(e.target.value);
+        }}
+      />
+      <button onClick={addTodo}>Add</button>
+    </div>
+  );
+}
